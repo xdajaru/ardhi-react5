cb4e8427261d66310b152783dbc39083b1a8d4f5
xdajaru <ardhiwinahyuputra@gmail.com>
2022-08-19 14:19:11 +0700

    Maumakanh

--- src/components/Makanan/JenisMakanan/MealItem.js
+++ src/components/Makanan/JenisMakanan/MealItem.js
@@ -1,9 +1,17 @@
+import { useContext } from "react";
+
 import classes from "./MealItem.module.css";
 
 import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = function (props) {
+	const cartCtx = useContext(CartContext);
 	const price = `Rp. ${props.price}`;
+
+	const addToCartHandler = (amount) => {
+		cartCtx.addItem({ id: props.id, name: props.name, amount: amount, price: props.price });
+	};
 
 	return (
 		<li className={classes.meal}>
@@ -13,7 +21,7 @@ const MealItem = function (props) {
 				<div className={classes.price}>{price}</div>
 			</div>
 			<div>
-				<MealItemForm id={props.id} />
+				<MealItemForm onAddToCart={addToCartHandler} />
 			</div>
 		</li>
 	);
--- src/components/Makanan/JenisMakanan/MealItemForm.js
+++ src/components/Makanan/JenisMakanan/MealItemForm.js
@@ -1,12 +1,30 @@
+import { useRef, useState } from "react";
 import classes from "./MealItemForm.module.css";
 
 import Input from "../../UI/Input";
 
 const MealItemForm = function (props) {
+	const [amountIsValid, setAmountIsValid] = useState(true);
+	const amountInputRef = useRef();
+
+	const submitHandler = (event) => {
+		event.preventDefault();
+
+		const enteredAmount = amountInputRef.current.value;
+		const enteredAmountNumber = +enteredAmount;
+
+		if (enteredAmount.trim().length === 0 || enteredAmountNumber < 1 || enteredAmountNumber > 5) {
+			setAmountIsValid(false);
+			return;
+		}
+
+		props.onAddToCart(enteredAmountNumber);
+	};
 	return (
-		<form className={classes.form}>
-			<Input label="Jumlah" input={{ id: "jumlah" + props.id, type: "number", min: "1", max: "5", step: "1", defaultValue: "1" }} />
+		<form className={classes.form} onSubmit={submitHandler}>
+			<Input ref={amountInputRef} label="Jumlah" input={{ id: "jumlah" + props.id, type: "number", min: "1", max: "5", step: "1", defaultValue: "1" }} />
 			<button>+ Tambah ke keranjang</button>
+			{!amountIsValid && <p>Tolong masukkan angka dari 1 - 5.</p>}
 		</form>
 	);
 };
